f38789c78c248f1f12981132a6eb54df8161bd94
Rosa <rosalie.boulard@gmail.com>
2024-02-13 15:41:02 +0100

    learning node ?

---
+++ src/app/lib/placeholder-data.js
@@ -0,0 +1,18 @@
+const users = [
+    {
+        id: 0,
+        name: 'God',
+        email: 'god@nextmail.com',
+        password: '123456789',
+    },
+];
+
+const tasks = [
+    {
+        id: 1,
+        taskname: 'Create an App',
+        priority: 'Urgent',
+        deadline: '01-03-2024',
+        done: false,
+    },
+];
