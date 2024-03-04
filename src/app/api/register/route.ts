01eb09ee1e2c3ca4d1a6a460fdfb983621f3bb3d
Rosa <rosalie.boulard@gmail.com>
2024-03-04 09:29:01 +0100

    meh

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,14 +1,14 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-export async function POST(request: Request) {
-  try {
-    const { email, password } = await request.json();
-    // YOU MAY WANT TO ADD SOME VALIDATION HERE
+// export async function POST(request: Request) {
+//   try {
+//     const { email, password } = await request.json();
+//     // YOU MAY WANT TO ADD SOME VALIDATION HERE
 
-    console.log({ email, password });
-  } catch (e) {
-    console.log({ e });
-  }
+//     console.log({ email, password });
+//   } catch (e) {
+//     console.log({ e });
+//   }
 
-  return NextResponse.json({ message: "success" });
-}
+//   return NextResponse.json({ message: "success" });
+// }
